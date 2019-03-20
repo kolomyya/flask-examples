c3c931a991be90575d85e27fa5dc441c296ec9d9
kolomyya <andriyzn4@gmail.com>
2019-03-20 18:49:18 -0500

    first commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,18 @@
 node {
     properties([parameters([string(defaultValue: '127.0.0.1', description: 'Plearse give IP to build the site', name: 'IP', trim: false)])])
     stage("Install git") {
-        sh 'ssh ec2-user@IP    sudo yum install  python-pip git -y'
+        sh "ssh ec2-user@${IP}    sudo yum install  python-pip git -y"
     }
     stage("Clone repo") {
         git 'https://github.com/kolomyya/flask-examples.git'
     }
     stage("Copy files") {
-        sh  "scp * ec2-user@IP:/tmp/"
+        sh  "scp * ec2-user${IP} :/tmp/"
     }
     stage("Intall requirements") {
-        sh "ssh ec2-user@IP   sudo pip install -r /tmp/requirements.txt"
+        sh "ssh ec2-user${IP}    sudo pip install -r /tmp/requirements.txt"
     }
     stage("Run App") {
-        sh 'ssh ec2-user@IP  python /tmp/01-hello-world'
+        sh "ssh ec2-user${IP}   python /tmp/01-hello-world"
     }
 }
